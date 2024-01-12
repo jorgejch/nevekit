ebd776e53a1015857750d78e62c56fe9f54afd37
Jorge Haddad <jorgejch@gmail.com>
2024-01-11 17:45:12 -0800

    Remove unused import and format test_get_character_portrait_success() function

--- nevekit/image/test_image.py
+++ nevekit/image/test_image.py
@@ -1,7 +1,8 @@
 # Test file for the image module.
 
+
 def test_get_character_portrait_success():
-  """
-  Test that the get_character_portrait method returns the correct value.
-  """
-  pass
+    """
+    Test that the get_character_portrait method returns the correct value.
+    """
+    pass
