08763984c8b8d24a50f451c48ebb4897c9209df2
Jorge Haddad <jorgejch@gmail.com>
2024-01-12 23:14:16 -0800

    Update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'bravado[fido]==11.0.3',
-    'bravado-core==6.1.1'
+    "bravado[fido]==11.0.3,<12.0.0",
+    "bravado-core>=6.1.1,<7.0.0",
+    "numpy>=1.26,<2.0",
+    "dill>=0.3.7,<1.0.0",
 ]
 
 setup(
-    name='nevekit',
-    version='0.1.0',
+    name="nevekit",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=requirements,
 )
