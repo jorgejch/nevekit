24ac8562c5072d767d1d850f01c3f4c6c7f27ca4
Jorge Haddad <jorgejch@gmail.com>
2024-01-14 00:49:43 -0800

    Add logger and test for getting character public info

--- nevekit/esi/test_esi_integration.py
+++ nevekit/esi/test_esi_integration.py
@@ -1,7 +1,7 @@
 # Integration test file for the sde module.
 import pytest
 import os
-from nevekit import get_nevekit_home
+from nevekit import get_nevekit_home, logger
 from nevekit.esi import ESI, SWAGGER_CLIENT_FILE_NAME
 
 try:
@@ -10,14 +10,12 @@ except KeyError:
     is_skip_test = True
 
 
+# Constants
+CHARACTER_ID = 2112002224
+
+
 @pytest.fixture()
 def esi():
-    # Remove the SwaggerClient pickle file if it exists.
-    try:
-        os.remove(f"{get_nevekit_home()}/{SWAGGER_CLIENT_FILE_NAME}")
-    except FileNotFoundError:
-        pass
-
     return ESI()
 
 
@@ -26,5 +24,33 @@ def test_init_success(esi):
     """
     Test that the ESI object is initialized correctly.
     """
+    # Remove the SwaggerClient pickle file if it exists.
+    try:
+        os.remove(f"{get_nevekit_home()}/{SWAGGER_CLIENT_FILE_NAME}")
+    except FileNotFoundError:
+        pass
+
     esi.init()
     assert esi.get_client() is not None
+
+
+@pytest.mark.skipif(is_skip_test, reason="Skipping the test")
+def test_get_character_public_info(esi):
+    """
+    Test using the SwaggerClient object to get a character's public info.
+    """
+
+    # Initialize the SwaggerClient object.
+    esi.init()
+
+    # Get the character standings.
+    character_public_info = (
+        esi.get_client()
+        .Character.get_characters_character_id(character_id=CHARACTER_ID)
+        .result()
+    )
+
+    logger.debug(f"character id {CHARACTER_ID} public info: {character_public_info}")
+
+    assert character_public_info is not None
+    assert character_public_info["name"] is not None
